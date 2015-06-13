f4e7fae92e5dac85c8ac603fc6f9872a0ba9ba31
Brian Mathiyakom <brian@rarevisions.net>
2015-06-12 22:57:31 -0700

    Initial "new" via rails-api.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+BagelCentralApi::Application.config.secret_token = '3a632f6e2de7fc3c317dc8da0722d552c444402fa1627f484912d93c8105faab084ded943ef16450c027078537798afba5c4edf76cf2cfb86be626ccafc3ce16'
