2a7246690d32de3920543f597caa9de39c9429bc
Brian Mathiyakom <brian@rarevisions.net>
2015-08-16 20:18:46 -0700

    Turn TeamService into TeamMaker.
    Focus on what TeamMaker was actualy doing, finding and making teams for
    a given player. This is to avoid having a collection of random actions
    on a TeamService object.

---
+++ app/services/team_maker.rb
@@ -0,0 +1,43 @@
+require 'set'
+
+class TeamMaker
+  def self.find_teams_for(player_id)
+    player_id = player_id.to_i
+    fail ArgumentError if player_id < 1
+
+    bagels = Bagel.with_player_id(player_id)
+      .includes(:owner, :teammate, :offensive_opponent, :defensive_opponent)
+
+    teams = form_teams(player_id, bagels)
+
+    teams.to_a
+  end
+
+  class << self
+    private
+
+    def form_teams(player_id, bagels)
+      teams = Set.new
+
+      bagels.each do |bagel|
+        if losing_team?(player_id, bagel)
+          teams << Team.new(defense: bagel.owner, offense: bagel.teammate)
+        elsif winning_team?(player_id, bagel)
+          teams << Team.new(offense: bagel.offensive_opponent,
+                            defense: bagel.defensive_opponent)
+        end
+      end
+
+      teams
+    end
+
+    def losing_team?(player_id, bagel)
+      player_id == bagel.owner.id || player_id == bagel.teammate.id
+    end
+
+    def winning_team?(player_id, bagel)
+      player_id == bagel.offensive_opponent.id ||
+        player_id == bagel.defensive_opponent.id
+    end
+  end
+end
--- test/services/team_maker_test.rb
+++ test/services/team_maker_test.rb
@@ -1,26 +1,26 @@
 require "test_helper"
 
-class TeamServiceTest < ActiveSupport::TestCase
+class TeamMakerTest < ActiveSupport::TestCase
   def test_find_teams_for_invalid_player_id
     assert_raises(ArgumentError, "negative id") {
-      TeamService.find_teams_for(-1)
+      TeamMaker.find_teams_for(-1)
     }
 
     assert_raises(ArgumentError, "zero") {
-      TeamService.find_teams_for(0)
+      TeamMaker.find_teams_for(0)
     }
   end
 
   def test_find_teams_for_player_with_no_bagels
     player = create(:player)
-    actual = TeamService.find_teams_for(player.id)
+    actual = TeamMaker.find_teams_for(player.id)
 
     assert(actual.empty?)
   end
 
   def test_find_teams_for_player_id
     player = build(:alice)
-    actual = TeamService.find_teams_for(player.id)
+    actual = TeamMaker.find_teams_for(player.id)
 
     refute(actual.empty?)
     actual.each do |team|
